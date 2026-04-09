c81372eddd28532f7333be04a0e42108afef836c
chrisliu6669 <2860402955@qq.com>
2026-04-09 13:25:49 +0800

    21111111111111

--- homework2.py
+++ homework2.py
@@ -4,7 +4,7 @@ matplotlib.use('TkAgg')
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 3 * np.pi, 0.1)
+x = np.arange(0,3 * np.pi,0.1)
 y_sin = np.sin(x)
 y_cos = np.cos(x)
 
